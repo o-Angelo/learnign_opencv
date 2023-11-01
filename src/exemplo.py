7ba4c4bfb192a605764d31c03f6839968490c6ea
o-Anjelo <angeloban92@gmail.com>
2023-11-01 11:52:01 -0300

    Alter Colors

--- src/exemplo.py
+++ src/exemplo.py
@@ -7,7 +7,25 @@ def showImg(img):
     plt.imshow(img)
     plt.show()
 
+def getColor(img, x, y):
+    return img.item(y,x,0), img.item(y,x,1), img.item(y,x,2)
+
+def setColor(img, x, y, b, g, r):
+    img.itemset((y, x, 0), b)
+    img.itemset((y, x, 1), g)
+    img.itemset((y, x, 2), r)
+    return img
 
 def main():
     obj_img = cv2.imread("./imgs/pessoa.jpg")
+    altura, largura, cor = obj_img.shape
+    print("Dimensões da Imagem: "+str(altura)+", "+str(largura))
+    print("Canais de Cores: "+str(cor))
 
+    for y in range(0, altura):
+        for x in range(0, largura):
+            azul, verde, vermelho = getColor(obj_img, x, y)
+            obj_img = setColor(obj_img, x, y, 0, 0, vermelho)
+    
+    showImg(obj_img)
+main()
